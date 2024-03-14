e219d140a45b528997f766d775f951acbeb4a315
TiagoCostaKM <Tiago.Simoes@konicaminolta.pt>
2024-03-14 15:58:34 +0000

    minor fix

--- BB_API/Controllers/ShiptoController.cs
+++ BB_API/Controllers/ShiptoController.cs
@@ -1,23 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.Entity;
-using System.Data.SqlClient;
-using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Web.Http;
-using WebApplication1.App_Start;
 using WebApplication1.Models;
-using WebApplication1.Models.MIF_Renovations;
-using WebApplication1.Models.SLAs;
 
 namespace WebApplication1.Controllers
 {
-    public class Shipto : ApiController
+    public class ShiptoController : ApiController
     {
         [AcceptVerbs("GET", "POST")]
-        [ActionName("GetAllDeliverLocations")]
-        public IHttpActionResult GetAllDeliverLocations(string NIF)
+        [ActionName("GetAllDeliveryLocations")]
+        public IHttpActionResult GetAllDeliveryLocations(string NIF)
         {
             try
             {
