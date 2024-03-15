c1acf310106b46fe2ce6e1575cd8f615ad0a5a4d
TiagoCostaKM <Tiago.Simoes@konicaminolta.pt>
2024-03-15 17:34:27 +0000

    minorfix

--- BB_API/Models/BB_LocaisEnvio.cs
+++ BB_API/Models/BB_LocaisEnvio.cs
@@ -34,7 +34,6 @@ namespace WebApplication1.Models
         public string BusinessCode { get; set; }
         public string Customer1 { get; set; }
         public string Customer2 { get; set; }
-        public string StreetAndNr { get; set; }
         public string Country { get; set; }
         public string RoadType { get; set; }
         public string RoadName { get; set; }
