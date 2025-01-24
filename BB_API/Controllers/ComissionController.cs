995b4408ee6dc85efe7743e2e4babfdd87461c70
TiagoCostaKM <Tiago.Simoes@konicaminolta.pt>
2025-01-24 17:04:31 +0000

    minor fixes

--- BB_API/Controllers/ComissionController.cs
+++ BB_API/Controllers/ComissionController.cs
@@ -1150,13 +1150,12 @@ namespace WebApplication1.Controllers
                     bb_commission_general.CN_PRS = basket.Where(x => x.Family.Contains("PRS")).Sum(x => x.TotalNetsale);
                     bb_commission_general.CN_MCS_BPS = basket.Where(x => x.Family.Contains("MCS") || x.Family.Contains("BPS")).Sum(x => x.TotalNetsale);
 
-                    bb_commission_general.GP_Total = bb_commission_general.GP_Hard;
-
                     // Soma de todos os GP daquele proposalID (incluindo RS)
                     bb_commission_general.GP_Hard = profitDictionary.Where(d => d.Key != "HW" && d.Key != "MOBOTIX" && d.Key != "PPHW" && d.Key != "MCS" && d.Key != "IMS" && d.Key != "BPS").Sum(x => x.Value.GPTotal);
                     bb_commission_general.GP_IMS_VSS = profit_IMS_VSS.GPTotal + profit_MOBOTIX.GPTotal;
                     bb_commission_general.GP_PRS = profit_PRS.GPTotal;
                     bb_commission_general.GP_MCS_BPS = profit_MCS_BPS.GPTotal;
+                    bb_commission_general.GP_Total = bb_commission_general.GP_Hard;
 
                     bb_commission_general.Incidencias = null;
                     bb_commission_general.Es_Segunda_Mano = isSecondHand;
@@ -1258,7 +1257,15 @@ namespace WebApplication1.Controllers
                     // Calculo do Percentage_GP ----------------------------------------------
                     if (bb_commission_general.CN_Total > 0)
                     {
-                        bb_commission_general.Percentage_GP = ((bb_commission_general.GP_Total / bb_commission_general.CN_Total) * 100).ToString() + "%";
+                        var percentage_GP = ((bb_commission_general.GP_Total / bb_commission_general.CN_Total) * 100);
+                        if(percentage_GP != null)
+                        {
+                            bb_commission_general.Percentage_GP = percentage_GP.ToString() + '%';
+                        }
+                        else
+                        {
+                            bb_commission_general.Percentage_GP = "-";
+                        }
                     }
                     else
                     {
@@ -1268,7 +1275,15 @@ namespace WebApplication1.Controllers
                     // Calculo do Percentage_Comision ----------------------------------------
                     if (bb_commission_general.GP_Total > 0)
                     {
-                        bb_commission_general.Percentage_Comision = ((bb_commission_general.Comision / bb_commission_general.GP_Total) * 100).ToString() + "%";
+                        var percentage_Comision = ((bb_commission_general.Comision / bb_commission_general.GP_Total) * 100);
+                        if (percentage_Comision != null)
+                        {
+                            bb_commission_general.Percentage_Comision = percentage_Comision.ToString() + '%';
+                        }
+                        else
+                        {
+                            bb_commission_general.Percentage_Comision = "-";
+                        }
                     }
                     else
                     {
